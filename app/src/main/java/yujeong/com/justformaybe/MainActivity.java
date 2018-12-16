37095b224714535d7d027d7f853078f620aee185
yuyu413 <yuyu413@naver.com>
2018-12-16 21:01:17 +0900

    Wifi가 활성화 된 상태에서만 앱을 이용할 수 있도록 함

--- app/src/main/java/yujeong/com/justformaybe/MainActivity.java
+++ app/src/main/java/yujeong/com/justformaybe/MainActivity.java
@@ -1,9 +1,12 @@
 package yujeong.com.justformaybe;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -13,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startDetection(View view) {
-        Intent intent = new Intent(MainActivity.this, DetectActivity.class);
-        startActivity(intent);
+        WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        if (wifi != null && wifi.isWifiEnabled()){
+            Intent intent = new Intent(MainActivity.this, DetectActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Wifi가 활성화된 상태에서만 사용 가능합니다. ㅠ_ㅠ", Toast.LENGTH_LONG).show();
+        }
     }
 }
