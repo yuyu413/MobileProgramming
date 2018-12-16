1c7cd0007acd8d56e9dad5be4a32f7e9d4f9ce69
yuyu413 <yuyu413@naver.com>
2018-12-16 23:11:09 +0900

    상세 설명 페이지에서 타이틀 제거

--- app/src/main/java/yujeong/com/justformaybe/InfoActivity.java
+++ app/src/main/java/yujeong/com/justformaybe/InfoActivity.java
@@ -1,10 +1,11 @@
 package yujeong.com.justformaybe;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class InfoActivity extends AppCompatActivity {
+public class InfoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
