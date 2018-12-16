10cf56d2321be52420dbac5c51fbc3b19a6ef454
yuyu413 <yuyu413@naver.com>
2018-12-16 21:26:35 +0900

    실시간 탐지가 아닌 시작 버튼 누를 때 한번 결과를 가져옴

--- app/src/main/java/yujeong/com/justformaybe/DetectActivity.java
+++ app/src/main/java/yujeong/com/justformaybe/DetectActivity.java
@@ -3,12 +3,14 @@ package yujeong.com.justformaybe;
 import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -18,48 +20,39 @@ import java.util.List;
 import yujeong.com.justformaybe.core.CamDetectionReceiver;
 
 public class DetectActivity extends AppCompatActivity {
-    private WifiManager wifiManager;
     private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detect);
-
-        String[] PERMS_INITIAL={
-                Manifest.permission.ACCESS_FINE_LOCATION,
-        };
-        ActivityCompat.requestPermissions(this, PERMS_INITIAL, 127);
-
         context = getApplicationContext();
-        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        CamDetectionReceiver wifiScanReceiver = new CamDetectionReceiver(new CamDetectionReceiver.SuccessListener() {
-            @Override
-            public void whenSuccess(int result) {
-                Log.d("MAYBE", "result level: " + result);
-                if(result == CamDetectionReceiver.LEVEL_IN_DANGER) {
-                    Toast.makeText(context, "몰카 위험이 있습니다.", Toast.LENGTH_LONG).show();
-                }
 
-                if(result == CamDetectionReceiver.LEVEL_IN_WARNING) {
-                    Toast.makeText(context, "몰카로 의심되는 물체가 있습니다. 주의하세요", Toast.LENGTH_LONG).show();
-                }
+        int result = getIntent().getIntExtra("result", -1);
 
-                if(result == CamDetectionReceiver.LEVEL_SAFE) {
-                    Toast.makeText(context, "제가 진단한 바로는 안전한 환경입니다!", Toast.LENGTH_LONG).show();
-                }
-            }
+        if(result == CamDetectionReceiver.LEVEL_IN_DANGER) {
+            Toast.makeText(context, "몰카 위험이 있습니다.", Toast.LENGTH_LONG).show();
+        }
 
-            @Override
-            public void whenFailure(String whyFailed) {
-                Log.d("MAYBE", "Failed to scan wifi: " + whyFailed);
-            }
-        }, wifiManager);
+        if(result == CamDetectionReceiver.LEVEL_IN_WARNING) {
+            Toast.makeText(context, "몰카로 의심되는 물체가 있습니다. 주의하세요", Toast.LENGTH_LONG).show();
+        }
 
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        context.registerReceiver(wifiScanReceiver, intentFilter);
-        boolean success = wifiManager.startScan();
-        Log.d("MAYBE", "started to scan");
+        if(result == CamDetectionReceiver.LEVEL_SAFE) {
+            Toast.makeText(context, "제가 진단한 바로는 안전한 환경입니다!", Toast.LENGTH_LONG).show();
+        }
+
+        if(result == -1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("몰카 탐지 실패")
+                    .setMessage("몰카를 탐지하는데 실패했어요 ㅠ_ㅠ 다시 시도해주세요.")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            DetectActivity.this.finish();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
     }
 }
--- app/src/main/java/yujeong/com/justformaybe/MainActivity.java
+++ app/src/main/java/yujeong/com/justformaybe/MainActivity.java
@@ -1,14 +1,23 @@
 package yujeong.com.justformaybe;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import yujeong.com.justformaybe.core.CamDetectionReceiver;
+
 public class MainActivity extends AppCompatActivity {
+    private WifiManager wifiManager;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +27,35 @@ public class MainActivity extends AppCompatActivity {
     public void startDetection(View view) {
         WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         if (wifi != null && wifi.isWifiEnabled()){
-            Intent intent = new Intent(MainActivity.this, DetectActivity.class);
-            startActivity(intent);
+            String[] PERMS_INITIAL={
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+            };
+            ActivityCompat.requestPermissions(this, PERMS_INITIAL, 127);
+
+            context = getApplicationContext();
+            wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            CamDetectionReceiver wifiScanReceiver = new CamDetectionReceiver(new CamDetectionReceiver.SuccessListener() {
+                @Override
+                public void whenSuccess(int result) {
+                    Log.d("MAYBE", "result level: " + result);
+                    Intent intent = new Intent(MainActivity.this, DetectActivity.class);
+                    intent.putExtra("result", result);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void whenFailure(String whyFailed) {
+                    Log.d("MAYBE", "Failed to scan wifi: " + whyFailed);
+                }
+            }, wifiManager);
+
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+            context.registerReceiver(wifiScanReceiver, intentFilter);
+            boolean success = wifiManager.startScan();
+            if(!success) {
+                Toast.makeText(this, "처음에 뜬 권한 요청을 허용해주세요. ㅠ_ㅠ", Toast.LENGTH_LONG).show();
+            }
         } else {
             Toast.makeText(this, "Wifi가 활성화된 상태에서만 사용 가능합니다. ㅠ_ㅠ", Toast.LENGTH_LONG).show();
         }
